5b46ccddfd1840ee39936524a8bdea0ef1994727
LupertEverett <86886753+LupertEverett@users.noreply.github.com>
2023-04-20 23:50:42 +0300

    Replace critical() with information()

--- gui/FileEditWidget.py
+++ gui/FileEditWidget.py
@@ -131,7 +131,7 @@ class FileEditWidget(QWidget):
         pos = self.sci.SendScintilla(QsciScintilla.SCI_SEARCHINTARGET, len(text_to_find), text_to_find.encode("utf-8"))
 
         if pos == -1 and search_scope == SearchScope.SINGLE_FILE:
-            QMessageBox.critical(self.parent(), "Find/Replace", "End of file reached")
+            QMessageBox.information(self.parent(), "Find/Replace", "End of file reached")
             return pos
         if pos == -1 and search_scope == SearchScope.OPEN_TABS:
             self.move_to_tab_requested.emit(text_to_find, FindType.FIND_NEXT)
@@ -149,7 +149,7 @@ class FileEditWidget(QWidget):
         pos = self.sci.SendScintilla(QsciScintilla.SCI_SEARCHINTARGET, len(text_to_find), text_to_find.encode("utf-8"))
 
         if pos == -1 and search_scope == SearchScope.SINGLE_FILE:
-            QMessageBox.critical(self.parent(), "Find/Replace", "Beginning of file reached")
+            QMessageBox.information(self.parent(), "Find/Replace", "Beginning of file reached")
             return
         if pos == -1 and search_scope == SearchScope.OPEN_TABS:
             self.move_to_tab_requested.emit(text_to_find, FindType.FIND_PREVIOUS)
