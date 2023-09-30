1526849eeac6d1435982cfacc2401778f77536b0
LupertEverett <86886753+LupertEverett@users.noreply.github.com>
2023-09-30 22:52:28 +0300

    Overhaul the theming system, update the dark theme

--- data/ColorThemes.py
+++ data/ColorThemes.py
@@ -5,15 +5,19 @@ from . import IDESettings
 
 
 def query_color_themes() -> list[str]:
-    return glob.glob("*.css", root_dir="res/colorthemes/")
+    return [x.stem for x in Path("res/colorthemes").iterdir() if x.is_dir() and _is_valid_theme_folder(x)]
 
-def _is_color_theme_exists(color_theme_name: str) -> bool:
-    full_path = Path("res/colorthemes/{}".format(color_theme_name))
 
-    return full_path.exists() and not full_path.is_dir()
+def _is_valid_theme_folder(folder_path: Path):
+    theme_file_name = folder_path/"theme.css"
+
+    return theme_file_name.exists() and theme_file_name.is_file()
+
 
 def set_color_theme(color_theme_name: str):
-    if not _is_color_theme_exists(color_theme_name):
+    folder_path = Path("res/colorthemes/{}".format(color_theme_name))
+
+    if not _is_valid_theme_folder(folder_path):
         raise FileNotFoundError("Color theme {} not found!".format(color_theme_name))
 
     IDESettings.set_color_theme(color_theme_name)
@@ -22,10 +26,12 @@ def set_color_theme(color_theme_name: str):
 def get_color_theme_path() -> str:
     color_theme_name = IDESettings.get_color_theme()
 
-    if not _is_color_theme_exists(color_theme_name):
+    folder_path = Path("res/colorthemes/{}".format(color_theme_name))
+
+    if not _is_valid_theme_folder(folder_path):
         raise FileNotFoundError("Color theme {} not found!".format(color_theme_name))
 
-    return "res/colorthemes/{}".format(color_theme_name)
+    return "res/colorthemes/{}/theme.css".format(color_theme_name)
 
 
 def load_current_color_theme() -> str:
