eac579127ea4de2d9a2c01244166159e89ee144c
LupertEverett <86886753+LupertEverett@users.noreply.github.com>
2024-05-26 18:44:46 +0300

    Force WindowsVista QtStyle on Windows

--- PyWrightIDE.py
+++ PyWrightIDE.py
@@ -6,6 +6,11 @@ if __name__ == '__main__':
 
     app = QApplication(sys.argv)
 
+    # App style looks horrible when directly run from Windows for some reason, this works around that
+    # Perhaps switching to Qt6 was a mistake
+    if sys.platform == "win32":
+        app.setStyle("WindowsVista")
+
     main_window = IDEMainWindow()
 
     main_window.show()
