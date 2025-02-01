9e21acfc9bbb923702d59d87748e676348002bdd
LupertEverett <86886753+LupertEverett@users.noreply.github.com>
2025-02-01 20:00:47 +0300

    Redirect the user to Welcome dialog if the last open game folder goes missing

--- PyWrightIDE.py
+++ PyWrightIDE.py
@@ -3,6 +3,7 @@ if __name__ == '__main__':
     from gui.IDEMainWindow import IDEMainWindow
     from gui.WelcomeDialog import WelcomeDialog
     from data import IDESettings
+    from pathlib import Path
 
     import sys
 
@@ -13,7 +14,12 @@ if __name__ == '__main__':
     if sys.platform == "win32":
         app.setStyle("WindowsVista")
 
-    if not IDESettings.get_autoload_last_game_check():
+    # Check if the autoload path exists and is still a valid path
+    # If that's the case, then launch the IDE, otherwise launch the Welcome dialog instead
+    game_folder_path = Path(IDESettings.get_autoload_last_game_path())
+    game_folder_exists = game_folder_path.exists() and game_folder_path.is_dir()
+
+    if not IDESettings.get_autoload_last_game_check() or not game_folder_exists:
         welcome_dialog = WelcomeDialog()
         if welcome_dialog.exec():
             main_window = IDEMainWindow(welcome_dialog.get_selected_folder_path())
