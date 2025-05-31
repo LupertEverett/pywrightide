6e6d2837a0bff5a50dae660ece3bdb69e700c9f9
LupertEverett <86886753+LupertEverett@users.noreply.github.com>
2025-05-31 21:18:47 +0300

    MainWindowStatusBar: Remove unintended import and custom separators

--- gui/MainWindowStatusBar.py
+++ gui/MainWindowStatusBar.py
@@ -1,7 +1,6 @@
 """Just a status bar with a custom separator"""
 
 from PyQt6.QtWidgets import QStatusBar, QLabel, QFrame
-from inputremapper.gui.components.main import StatusBar
 
 
 class MainWindowStatusBar(QStatusBar):
@@ -12,25 +11,19 @@ class MainWindowStatusBar(QStatusBar):
         self.setSizeGripEnabled(False)
 
         self._selection_length_info_label = QLabel()
-        self._selection_info_separator = StatusBarSeparator()
         self._line_col_info_label = QLabel()
-        self._line_col_info_separator = StatusBarSeparator()
         self._installation_path_label = QLabel("No PyWright folder selected")
 
         self.addPermanentWidget(self._selection_length_info_label)
-        self.addPermanentWidget(self._selection_info_separator)
         self.addPermanentWidget(self._line_col_info_label)
-        self.addPermanentWidget(self._line_col_info_separator)
         self.addPermanentWidget(self._installation_path_label)
 
         self.setContentsMargins(4, 2, 4, 2)
 
     def set_selection_length_info(self, selection_length: int):
         if selection_length > 0:
-            self._selection_info_separator.setHidden(False)
             self._selection_length_info_label.setText("{} characters selected".format(selection_length))
         else:
-            self._selection_info_separator.setHidden(True)
             self._selection_length_info_label.setText("")
 
     def set_installation_path_info(self, installation_path: str):
@@ -42,15 +35,6 @@ class MainWindowStatusBar(QStatusBar):
         :param line: Line number
         :param col: Column number"""
         if line > 0 and col > 0:
-            self._line_col_info_separator.setHidden(False)
             self._line_col_info_label.setText("Ln: {}, Col: {}".format(line, col))
         else:
-            self._line_col_info_separator.setHidden(True)
             self._line_col_info_label.setText("")
-
-
-class StatusBarSeparator(QFrame):
-    """Literally just a vertical line. Why QStatusBar simply doesn't have an addSeparator() method is anyone's guess."""
-    def __init__(self, parent=None):
-        super().__init__()
-        self.setFrameStyle(QFrame.Shape.VLine)
