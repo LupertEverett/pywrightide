9c52e6aec1bd448d4ea93d2cc6c9b7e4fbe4bc42
LupertEverett <86886753+LupertEverett@users.noreply.github.com>
2025-11-15 22:28:09 +0300

    PyWrightGamePathItem: Two small changes: Use title field as the game's title if it exists, otherwise fallback to folder name Use the actual path of the game instead of its parent path

--- data/PyWrightGamePathItem.py
+++ data/PyWrightGamePathItem.py
@@ -21,23 +21,28 @@ class PyWrightGamePathItem(QStandardItem):
         super().__init__()
 
         self.path_str = path_str
-        self.parent_path = Path(self.path_str).parent
-        self.game_name = Path(self.path_str).stem
 
-        # Try to load the game's icon
-        # Use the fallback icon if the loading fails
         try:
             (title, author, version, icon_path) = PyWrightGameInfo.get_game_data_info(Path(path_str))
+            # Try to load the game's icon
+            # Use the fallback icon if loading fails
             if str(icon_path) != "":
                 icon_pixmap = QPixmap(str(icon_path))
             else:
                 icon_pixmap = QPixmap(FALLBACK_GAME_ICON_PATH)
+
+            # Try to load the game's title
+            # Use the folder name if loading fails
+            if title != "":
+                self.game_name = title
+            else:
+                self.game_name = Path(self.path_str).stem
         except FileNotFoundError:
             author = "Unknown Author"
             icon_pixmap = QPixmap(FALLBACK_GAME_ICON_PATH)
 
         author_text = "By " + author if author != "" else ""
-        path_text = TEXT_HTML.format(self.game_name, author_text, self.parent_path)
+        path_text = TEXT_HTML.format(self.game_name, author_text, self.path_str)
         self.setText(path_text)
 
         icon_pixmap = icon_pixmap.scaled(64, 64,
