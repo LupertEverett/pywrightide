32f12b4486226cd722be205312ff305a2dfb1527
LupertEverett <86886753+LupertEverett@users.noreply.github.com>
2026-03-19 01:47:13 +0300

    Add FolderBrowserView

---
+++ gui/FolderBrowserView.py
@@ -0,0 +1,275 @@
+from pathlib import Path
+
+from PyQt6.QtCore import pyqtSignal, QModelIndex, Qt, QUrl
+from PyQt6.QtGui import QFileSystemModel, QMouseEvent, QAction, QDesktopServices
+from PyQt6.QtWidgets import QTreeView, QFileIconProvider, QMenu, QMessageBox, QInputDialog, QLineEdit
+
+from data.PyWrightCase import PyWrightCase
+from data.PyWrightGame import PyWrightGameInfo
+from gui.CasePropertiesEditorDialog import CasePropertiesEditorDialog
+
+important_files = ("data.txt", "intro.txt", "evidence.txt")
+non_sub_scriptable_extensions = (".mcro",
+                                 ".examine.txt", ".move.txt", ".talk.txt", ".present.txt",
+                                 "menu.txt", "menu.script.txt")
+# Any folder that isn't these are gonna be treated as case folders (for now?)
+non_case_folders = ("art", "music", "movies", "sfx")
+
+
+class FolderBrowserView(QTreeView):
+    """Custom Tree View that has the ability to unselect items when clicked on an empty area"""
+    open_new_tab = pyqtSignal(str)
+    file_renamed_from_browser_view = pyqtSignal(str, str)
+
+    def __init__(self, parent = None):
+        super().__init__(parent)
+
+        self.doubleClicked.connect(self._handle_tree_view_double_clicked)
+        self.setContextMenuPolicy(Qt.ContextMenuPolicy.CustomContextMenu)
+        self.customContextMenuRequested.connect(self._handle_view_context_menu)
+        self.doubleClicked.connect(self._handle_tree_view_double_clicked)
+
+        self._selected_game_info : PyWrightGameInfo | None = None
+
+    def select_pywright_game(self, selected_game_info: PyWrightGameInfo):
+        fs_model = QFileSystemModel()
+        icon_provider = QFileIconProvider()
+
+        fs_model.setIconProvider(icon_provider)
+
+        name_filter = ["*.txt", "*.mcro"]
+        fs_model.setNameFilters(name_filter)
+        fs_model.setNameFilterDisables(False)
+
+        self._selected_game_info = selected_game_info
+
+        self.setModel(fs_model)
+        self.setRootIndex(fs_model.setRootPath(str(selected_game_info.game_path)))
+        self.setIndentation(20)
+        self.setSortingEnabled(True)
+        self.sortByColumn(0, Qt.SortOrder.AscendingOrder)
+        self.header().hide()
+        self.hideColumn(1)
+        self.hideColumn(2)
+        self.hideColumn(3)
+
+    def clear_root_path(self):
+        self.setModel(None)
+
+    def _handle_tree_view_double_clicked(self, index: QModelIndex):
+        item_path = QFileSystemModel().filePath(index)
+        if not Path(item_path).is_dir():
+            self.open_new_tab.emit(item_path)
+
+    def _handle_view_context_menu(self, position):
+        indexes = self.selectedIndexes()
+
+        menu = QMenu()
+
+        new_case_action = QAction("Add New Case", self)
+        new_case_action.triggered.connect(self._handle_add_new_case_action)
+        new_case_action.setStatusTip("Add a new case to the current game")
+
+        new_script_action = QAction("Add New Script", self)
+        new_script_action.triggered.connect(self._handle_new_script_action)
+        new_script_action.setStatusTip("Add a new script to the selected case folder")
+
+        open_dir_action = QAction("Open Folder in File Manager", self)
+        open_dir_action.triggered.connect(self._handle_open_dir_action)
+        open_dir_action.setStatusTip("Open selected folder in the default File Manager")
+
+        rename_file_action = QAction("Rename", self)
+        rename_file_action.triggered.connect(self._handle_rename_file_action)
+        rename_file_action.setStatusTip("Rename selected item")
+
+        remove_file_action = QAction("Remove", self)
+        remove_file_action.triggered.connect(self._handle_remove_file_action)
+        remove_file_action.setStatusTip("Remove selected item")
+
+        create_examine_script_action = QAction("Create Examine Script", self)
+        create_examine_script_action.triggered.connect(lambda: self._create_sub_script_file("examine"))
+        create_examine_script_action.setStatusTip("Create Examine script for the selected scene")
+
+        create_move_script_action = QAction("Create Move Script", self)
+        create_move_script_action.triggered.connect(lambda: self._create_sub_script_file("move"))
+        create_move_script_action.setStatusTip("Create Move script for the selected scene")
+
+        create_present_script_action = QAction("Create Present Script", self)
+        create_present_script_action.triggered.connect(lambda: self._create_sub_script_file("present"))
+        create_present_script_action.setStatusTip("Create Present script for the selected scene")
+
+        create_talk_script_action = QAction("Create Talk Script", self)
+        create_talk_script_action.triggered.connect(lambda: self._create_sub_script_file("talk"))
+        create_talk_script_action.setStatusTip("Create Talk script for the selected scene")
+
+        if len(indexes) > 0:
+            index = self.selectedIndexes()[0]
+            model: QFileSystemModel = index.model()
+
+            file_name = model.fileName(index)
+
+            if model.isDir(index) and model.fileName(index) not in non_case_folders:
+                menu.addAction(new_script_action)
+            elif file_name.endswith((".txt", ".mcro")) and file_name not in important_files:
+                if not file_name.endswith(non_sub_scriptable_extensions):
+                    menu.addAction(create_examine_script_action)
+                    menu.addAction(create_move_script_action)
+                    menu.addAction(create_present_script_action)
+                    menu.addAction(create_talk_script_action)
+                menu.addAction(rename_file_action)
+                menu.addAction(remove_file_action)
+            menu.addSeparator()
+        menu.addAction(new_case_action)
+
+        if len(indexes) == 0 or model.isDir(index):
+            menu.addSeparator()
+            menu.addAction(open_dir_action)
+
+        menu.exec(self.mapToGlobal(position))
+
+    def _handle_add_new_case_action(self):
+        add_dialog = CasePropertiesEditorDialog(self._selected_game_info, None, self)
+
+        if add_dialog.exec():
+            self._selected_game_info.create_new_case(add_dialog.get_case())
+
+    def _handle_new_script_action(self):
+        index = self.selectedIndexes()[0]
+        model: QFileSystemModel = index.model()
+
+        if not model.isDir(index):
+            return
+
+        dir_path = model.filePath(index)
+
+        name, ok = QInputDialog.getText(self, "New Script", "Enter file name", QLineEdit.EchoMode.Normal)
+
+        if ok and name != "":
+            if not name.endswith((".mcro", ".txt")):
+                name = name + ".txt"
+            final_path = Path("{}/{}".format(dir_path, name))
+            if final_path.exists():
+                QMessageBox.critical(self, "Error", "File already exists!", QMessageBox.StandardButton.Ok)
+                return
+
+            with open(final_path, "x"):
+                pass
+
+    def _create_sub_script_file(self, sub_script_name: str):
+        index = self.selectedIndexes()[0]
+        model: QFileSystemModel = index.model()
+
+        dir_path = Path("{}/..".format(model.filePath(index)))
+        file_name = model.fileName(index)
+
+        # Analyze if the file has an extra .script suffix first
+        script_name_sections = file_name.split(sep=".")
+
+        if len(script_name_sections) < 2:
+            return
+
+        base_script_name = script_name_sections[0]
+
+        resulting_script_name = base_script_name + "." + sub_script_name + ".txt"
+
+        menu_script_name = base_script_name + "menu"
+        menu_script_name += ("." + script_name_sections[1]) if script_name_sections[1].lower() == "script" else ""
+        menu_script_name += ".txt"
+
+        menu_script_full_path = dir_path/menu_script_name
+
+        if not (menu_script_full_path.is_file() and menu_script_full_path.exists()):
+            with open(menu_script_full_path, "x"):
+                pass
+
+        full_file_path = dir_path/resulting_script_name
+
+        if full_file_path.exists() and full_file_path.is_file():
+            QMessageBox.critical(self, "Error", "Script named \"{}\" already exists!".format(resulting_script_name),
+                                 QMessageBox.StandardButton.Ok)
+            return
+
+        with open(full_file_path, "w"):
+            pass
+
+    def _handle_open_dir_action(self):
+        if len(self.selectedIndexes()) == 0:
+            # Open the Game's root dir instead.
+            QDesktopServices.openUrl(QUrl.fromLocalFile(str(self._selected_game_info.game_path)))
+            return
+
+        index = self.selectedIndexes()[0]
+        model: QFileSystemModel = index.model()
+        dir_path = model.filePath(index)
+        QDesktopServices.openUrl(QUrl.fromLocalFile(dir_path))
+
+    def _handle_rename_file_action(self):
+        if len(self.selectedIndexes()) == 0:
+            return
+
+        index = self.selectedIndexes()[0]
+        model: QFileSystemModel = index.model()
+        was_read_only = model.isReadOnly()
+        old_name = model.fileName(index)
+
+        dir_path = model.filePath(index)
+
+        if old_name in important_files:
+            QMessageBox.critical(self, "Error", "Important file \"{}\" cannot be renamed!".format(old_name),
+                                 QMessageBox.StandardButton.Ok)
+            return
+
+        new_name, ok = QInputDialog.getText(self, "Rename File", "Enter file name",
+                                            QLineEdit.EchoMode.Normal, old_name)
+
+        if not ok:
+            return
+
+        if new_name == "":
+            QMessageBox.critical(self, "Error", "Name cannot be empty!", QMessageBox.StandardButton.Ok)
+            return
+
+        if new_name == old_name:
+            QMessageBox.critical(self, "Error", "New name cannot be the same as old name!",
+                                 QMessageBox.StandardButton.Ok)
+            return
+
+        # dir_path contains the full path, including the name, so we gotta "move up" to get its directory.
+        final_path = Path("{}/../{}".format(dir_path, new_name))
+
+        if final_path.exists() and final_path.is_file():
+            QMessageBox.critical(self, "Error", "File \"{}\" already exists!".format(final_path.name),
+                                 QMessageBox.StandardButton.Ok)
+            return
+
+        model.setReadOnly(False)
+        model.setData(index, new_name)
+        model.setReadOnly(was_read_only)
+
+    def _handle_remove_file_action(self):
+        if len(self.selectedIndexes()) == 0:
+            return
+
+        index = self.selectedIndexes()[0]
+        model: QFileSystemModel = index.model()
+
+        if model.isDir(index):
+            return
+
+        file_name = model.fileName(index)
+
+        if file_name in important_files:
+            QMessageBox.critical(self, "Error", "Important file \"{}\" cannot be removed!".format(file_name),
+                                 QMessageBox.StandardButton.Ok)
+            return
+
+        prompt = QMessageBox.question(self, "Question", "Are you sure you want to remove \"{}\"?".format(file_name),
+                                      QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No,
+                                      QMessageBox.StandardButton.No)
+
+        if prompt == QMessageBox.StandardButton.Yes:
+            model.remove(index)
+
+    def mousePressEvent(self, event: QMouseEvent):
+        self.clearSelection()
+        QTreeView.mousePressEvent(self, event)
